a7e2c1bd42be9d9d4d63bdd333b96caca1dc1340
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-04-25 18:10:39 +0100

    Import from limetext/lime@7eb4511.

---
+++ ci/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+
+	# make sure we're up to date
+	sudo apt-get update -qq
+
+	# install go get dependencies
+	sudo apt-get install -qq mercurial
+
+	# install backend dependencies
+	sudo add-apt-repository -y ppa:fkrull/deadsnakes
+	sudo apt-get update -qq
+	sudo apt-get install -qq libonig-dev python3.4 python3.4-dev
+
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+
+	brew update
+	brew install oniguruma python3
+	ln -s "$(brew --prefix python3)/Frameworks/Python.framework/Versions/3.4/lib/pkgconfig/"* "$(brew --prefix)/lib/pkgconfig"
+
+else
+
+	echo "BUILD NOT CONFIGURED: $TRAVIS_OS_NAME"
+	exit 1
+
+fi
---
+++ ci/run_tests.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+source "$(dirname -- "$0")/setup.sh"
+
+function run_tests {
+	go test "$1" -covermode=count -coverprofile=tmp.cov
+	test_result=$?
+	# Can't do race tests at the same time as coverage as it'll report
+	# lots of false positives
+	go test -race "$1"
+	let test_result=$test_result+$?
+	echo -ne "${YELLOW}=>${RESET} test $1 - "
+	if [ "$test_result" == "0" ]; then
+		echo -e "${GREEN}SUCCEEDED${RESET}"
+	else
+		echo -e "${RED}FAILED${RESET}"
+	fi
+}
+
+function test_all {
+	let a=0
+	for pkg in $(go list "./$1/..."); do
+		run_tests "$pkg"
+		let a=$a+$test_result
+		if [ "$test_result" == "0" ]; then
+			sed 1d tmp.cov >> coverage.cov
+		fi
+		rm tmp.cov
+	done
+	test_result=$a
+}
+
+fold_start "get.cov" "get coverage tools"
+go get golang.org/x/tools/cmd/cover
+go get github.com/mattn/goveralls
+go get github.com/axw/gocov/gocov
+fold_end "get.cov"
+
+echo "mode: count" > coverage.cov
+
+ret=0
+
+fold_start "test" "run tests"
+test_all "$1"
+let ret=$ret+$test_result
+fold_end "test"
+
+if [ "$ret" == "0" ] && [ "$TRAVIS_OS_NAME" == "linux" ]; then
+	fold_start "coveralls" "post to coveralls"
+	"$(go env GOPATH | awk 'BEGIN{FS=":"} {print $1}')/bin/goveralls" -coverprofile=coverage.cov -service=travis-ci
+	fold_end "coveralls"
+fi
+
+rm coverage.cov
+
+exit $ret
