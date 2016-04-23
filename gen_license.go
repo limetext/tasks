f8ffb4c5dc7e720db6e7432534be8828590de0ce
Mohammad Zolfaghari <zidom72@gmail.com>
2016-04-24 02:13:10 +0430

    changed lime tasks we only need gen_license.go now

--- gen_license.go
+++ gen_license.go
@@ -2,43 +2,44 @@
 // Use of this source code is governed by a 2-clause
 // BSD-style license that can be found in the LICENSE file.
 
+// +build ignore
+
 package main
 
 import (
 	"bytes"
-	"go/format"
+	"errors"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strconv"
-	"strings"
 	"time"
 )
 
-var year = strconv.FormatInt(int64(time.Now().Year()), 10)
-var licenseheader = []byte(`// Copyright ` + year + ` The lime Authors.
+var (
+	year          = strconv.FormatInt(int64(time.Now().Year()), 10)
+	licenseheader = []byte(`// Copyright ` + year + ` The lime Authors.
 // Use of this source code is governed by a 2-clause
 // BSD-style license that can be found in the LICENSE file.
 `)
+)
 
-func patch(path string, fi os.FileInfo, err error) error {
+func license(path string, fi os.FileInfo, err error) error {
 	if fi.IsDir() {
 		switch filepath.Base(path) {
 		case "testdata":
 			return filepath.SkipDir
 		case "vendor":
 			return filepath.SkipDir
-		case "packages":
-			if !strings.Contains(path, "lib/") {
-				return filepath.SkipDir
-			}
 		}
 		return nil
 	}
 
 	switch filepath.Ext(path) {
-	case ".go", ".c", ".cpp":
+	case ".go":
 	default:
 		return nil
 	}
@@ -51,29 +52,28 @@ func patch(path string, fi os.FileInfo, err error) error {
 	lhn := append(licenseheader, '\n')
 	if !bytes.Equal([]byte("// Copyright"), cmp[:12]) {
 		cmp = append(lhn, cmp...)
-		log.Println("Added license to", path)
-		changed = true
-	}
-
-	fmt, err := format.Source(cmp)
-	if err != nil {
-		return err
-	}
-
-	if !bytes.Equal(cmp, fmt) {
-		log.Println("Formatted", path)
 		changed = true
 	}
 
 	if changed {
-		return ioutil.WriteFile(path, fmt, fi.Mode().Perm())
+		if *check {
+			return errors.New(fmt.Sprintf("Missing license in %s", path))
+		}
+		log.Println("Added license to", path)
+		return ioutil.WriteFile(path, cmp, fi.Mode().Perm())
 	}
 
 	return nil
 }
 
+var (
+	scan  = flag.String("scan", "./", "set scan path")
+	check = flag.Bool("check", false, "just check if all files have license")
+)
+
 func main() {
-	if err := filepath.Walk("./", patch); err != nil {
-		log.Println("done: ", err)
+	flag.Parse()
+	if err := filepath.Walk(*scan, license); err != nil {
+		log.Fatal(err)
 	}
 }
